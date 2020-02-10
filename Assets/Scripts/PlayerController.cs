2153c9b1cc55d8465810a61040f08fe00e4f01bf
Ruoyi Ye <roi.mihy@gmail.com>
2020-02-10 09:10:40 -0500

    Multiple new features added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Text lostText;
+
     public float force = 5;
     Rigidbody2D rb;
+    public Vector3 scaleChange = new Vector3(0.1f, 0.1f, 0);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,25 +22,43 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Debug.Log("Goodbye,World");
+        if (transform.position.x - transform.localScale.x/2 < -5 || transform.position.x + transform.localScale.x / 2 > 5 || transform.position.y - transform.localScale.y / 2 < -5 || transform.position.y + transform.localScale.y / 2 > 5)
+        {
+            Debug.Log("out of boarder");
+            lostText.text = "You Lost!";
+            lostText.gameObject.SetActive(true);
 
-        if (Input.GetKey(KeyCode.A))//if D is pressed
-        {
-            rb.AddForce(Vector2.left * force);// moce right by force
         }
-        if (Input.GetKey(KeyCode.S))//if D is pressed
+        else
         {
-            rb.AddForce(Vector2.right * force);// moce right by force
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+            rb.AddForce(movement * force);
+
+            //if (Input.GetKey(KeyCode.A))//if A is pressed
+            //{
+            //    rb.AddForce(Vector2.left * force);// moce right by force
+            //}
+            //if (Input.GetKey(KeyCode.S))//if S is pressed
+            //{
+            //    rb.AddForce(Vector2.right * force);// moce right by force
+            //}
+            //if (Input.GetKey(KeyCode.W))//if W is pressed
+            //{
+            //    rb.AddForce(Vector2.up * force);// moce right by force
+            //}
+            //if (Input.GetKey(KeyCode.Z))//if Z is pressed
+            //{
+            //    rb.AddForce(Vector2.down * force);// moce right by force
+            //}
         }
-        if (Input.GetKey(KeyCode.W))//if D is pressed
-        {
-            rb.AddForce(Vector2.up * force);// moce right by force
-        }
-        if (Input.GetKey(KeyCode.Z))//if D is pressed
-        {
-            rb.AddForce(Vector2.down * force);// moce right by force
-        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        transform.localScale += scaleChange;
     }
 }
--- Assets/Scripts/PrizeScript.cs
+++ Assets/Scripts/PrizeScript.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PrizeScript : MonoBehaviour
 {
+    public Text countText;
+    public Text winText;
+    public int goal;
+    private int count;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        count = 0;
+        countText.text = "Goal: " + count.ToString() + "/" + goal;
+
     }
 
     // Update is called once per frame
@@ -18,6 +26,22 @@ public class PrizeScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        transform.position = new Vector2(Random.Range(-6, 6), Random.Range(-4, 4));
+        if (count < goal - 1)
+        {
+            transform.position = new Vector3(Random.Range(-4, 4), Random.Range(-4, 4), -1);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            winText.text = "You Win!";
+            winText.gameObject.SetActive(true);
+        }
+        count = count + 1;
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Goal: " + count.ToString() + "/" + goal;
     }
 }
